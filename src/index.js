c34833ac6bd1ff45081153bfe45de163db15dacf
Ashish Singh Rawat <ashish9342@gmail.com>
2018-09-27 21:23:24 +0530

    video list completed

--- src/index.js
+++ src/index.js
@@ -1,22 +1,63 @@
-import React from 'react';
+import React, {
+    Component
+} from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import {
+    keys
+} from './config/key'
 
 import SearchBar from './components/search_bar';
+import VideoList  from './components/video_list';
+
+
+
+
 //creating a component
-
-
 // const App = function(){
 //     return <div>Hi!</div>
 //     // jsx
 // }
 
-const App = () => {
-    return (<div >
-        <SearchBar />        
-        </div>
-    );
-    // jsx
+// Downward data flow
+// parent component should be responsible for fetching data
+
+
+
+class App extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            videos: []
+        };
+
+        YTSearch({
+            key: keys.youtube,
+            term: 'reactjs'
+        },  (videos) =>{
+           this.setState({videos});
+        });
+    }
+
+    render() {
+        return ( <div>
+            <SearchBar />
+            < VideoList videos ={this.state.videos}/>
+            </div>
+        );
+    }
+
 }
+
+// const App = () => {
+//     return (<div >
+//         <SearchBar />        
+//         </div>
+//     );
+//     // jsx
+// }
 
 // take this component generated HTML and put it on the page(DOM)
 // React.render(App);
