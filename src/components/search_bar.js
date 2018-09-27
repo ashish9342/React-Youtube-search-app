a4d30a6d2dae060b06b8cf538fc853eefce5c897
Ashish Singh Rawat <ashish9342@gmail.com>
2018-09-27 06:57:31 +0530

    controlled components

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,7 +1,67 @@
-import React from 'react';
+import React, {
+  Component
+} from 'react';
 
-const SearchBar = () =>{
-  return <input/>;
+// functional component
+// const SearchBar = () =>{
+//   return <input/>;
+// }
+
+
+// class component
+// adding more functionality
+// class SearchBar extends React.Component {
+class SearchBar extends Component {
+
+
+
+
+  constructor(props) {
+    super(props);
+    // calling parent method
+
+    this.state = {
+      term: ''
+    }
+    //  State 
+    // Plain js object to react to user events
+    // exists in class component
+    // 
+  }
+
+
+  //must have in inside class component
+  render() {
+    // return <input onChange={this.onInputChange}/> ;
+    // es6 function
+    // return <input onChange = {
+    //   (event) => console.log(event.target.value)
+    // }
+
+    // setting up state
+    return (
+      <div>
+    
+    <input 
+    value ={this.state.term}
+    onChange = {
+      (event) => this.setState({
+        term: event.target.value
+      })
+    }
+    /> 
+    
+    
+    </div>
+    );
+
+  }
+  // controlled form input
+// Value of input : {this.state.term}
+  // onInputChange(event) {
+  //   console.log(event.target.value)
+  // }
+
 }
 
 export default SearchBar;
